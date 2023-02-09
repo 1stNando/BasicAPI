ba1f3e2ff59ba6a5c395553d17e9b396114a1e60
Fernando Aguilar <sparkmanperspective@gmail.com>
2023-02-08 19:09:04 -0500

    Made a FOR loop to be able to roll more than one die at once, and store the numbers in a List<>.

--- Controllers/DiceController.cs
+++ Controllers/DiceController.cs
@@ -12,17 +12,29 @@ namespace BasicAPI.Controllers
     public class DiceController : ControllerBase
     {
         [HttpGet]
-        public int Roll(int sides)
+        //count comes from the query parameter, sides comes from the GET request URL
+        //We also need to change the int data type associated with Roll() because it will 
+        //no longer return a simple "int" digit. Instead it will now implement the use of a List<> to show multiple dice simultaneously rolled. 
+        public int Roll(int sides, int count)
         {
+            //We added int count to the parameter so we can have MANY die at the same time.
+            //Make a space where the multiple "die" rolls can exist. 
+            var rolls = new List<int>();
+
             //Make a random number generator
             var randomNumberGenerator = new Random();
 
-            //Next(sides) would make a number between 0 and just less than sides
-            //so return that number + 1. Making the range from 1 to a number
-            //INCLUDING the value of sides.
-            var roll = randomNumberGenerator.Next(sides) + 1;
+            for (var rollNumber = 0; rollNumber < count; rollNumber++)
+            {
+                //Next(sides) would make a number between 0 and just less than sides
+                //so return that number + 1. Making the range from 1 to a number
+                //INCLUDING the value of sides.
+                var roll = randomNumberGenerator.Next(sides) + 1;
 
-            return roll;
+                rolls.Add(roll);
+            }
+
+            return rolls;
         }
     }
 }
